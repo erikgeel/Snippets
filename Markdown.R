973a60bf255221bf7666feaaecb174ce39520574
erikgeel <erikgeel@hotmail.com>
2018-03-23 11:23:40 +0100

    Mijn eerste aanpassingen

---
+++ Markdown.R
@@ -0,0 +1,15 @@
+#markdown = opmaaktaal voor html
+
+# [naam van de link] (http://www.adres.nl) zo krijg je link in html
+
+of
+ik lees veel van [r blogger][1]en [simple statist][2]!
+[1]: http://www.nu.nl "r blogger"
+[2]: http://kpn.com "simple statist"
+
+#voor documentatie klik http://rmarkdown.rstudio.com/ of
+#https://guides.github.com/features/mastering-markdown/
+
+https://daringfireball.net/projects/markdown/ #info
+#
+        
---
+++ Snippets/Noorzakelijke packedges.R
@@ -0,0 +1,68 @@
+# 27 feb 2018
+https://rpubs.com/williamsurles/# hier staat alle code van datacamp vd cursus
+  install.packages(c("installr", "knitr", "rmarkdown"))
+install.packages("installr")library(installr)
+install.packages("shiny")library(shiny) en ook install.packages("miniUI")
+install.packages("knitr")library(knitr)
+install.packages("rmarkdown")library(rmarkdown)
+install.packages("markdown")library(markdown)
+install.packages("googleVis")library(googleVis)
+install.packages("manipulate")library(manipulate)
+install.packages("tidyverse")# hoort bij pakket Lubridate
+install.packages("lubridate")library(lubridate) #datums
+install.packages("zoo")library(zoo)#datums moet nog thuis en linux
+install.packages("xts")library(xts)#datums moet nog thuis en linux
+install.packages("curl") library(curl)
+install.packages("data.table")library(data.table)
+install.packages("readr")library()
+install.packages("tidyr")library(tidyr)
+install.packages("swirl")library(swirl)
+install.packages("devtools")library(devtools)
+install.packages("xtable")library(xtable)
+install.packages("dply")library(dplyr)
+install.packages("ggplot2")library(ggplot2)
+install.packages("stringr")library(stringr)
+install.packages("plotly")library(plotly)
+install.packages("RCurl")library(RCurl)
+install.packages("latexpdf")library(latexpdf)
+install.packages("leaflet")library(leaflet)
+install_github('slidify', 'ramnathv')
+install_github('slidifyLibraries', 'ramnathv')library(slidify)
+install.packages("miniUI")library(miniUI)
+install.packages("readxl")library(readxl)
+install.packages("gdata")library(gdata) #can handle .xls files that are on the internet.
+install.packages("XLConnect")library(XLConnectJars) 
+install.packages("httr")library(httr)
+install.packages("rJava")library(rJava)
+install.packages("XLConnectJars")library()
+install.packages("jsonlite")library(jsonlite)
+install.packages("foreign")library()
+install.packages("haven")library()
+install.packages("RMySQL")library(RMySQL)
+install.packages("RPostgreSQL")library(RPostgreSQL)#moet nog en linux
+install.packages("DBI")library(DBI)
+install.packages("MASS")library(MASS)# niet noodzakelijk maar bevat reeks voorbeeld bestanden / oefen bestanden
+install.packages("car")library(car)# voor QQplot()functie ##moet nog thuis en mama
+install.packages("rpart")libraryrpart()#moet nog thuis en linux
+install.packages("corrplot")library(corrplot) #moet nog thuis en mama en linux
+install.packages("aplpack")library(aplpack)#moet nog thuis en mama en linux
+install.packages("corrplot")library(corrplot) #moet nog thuis en mama en linux
+install.packages("rpart")library()  #moet nog thuis en mama en linux
+install.packages("rvest")library(rvest)# voor webscraping url
+install.packages("ggmap")library(ggmap) #moet nog thuis en linux
+install.packages("sp")library(sp)#moet nog thuis en mama en linux
+install.packages("tmap")library(tmap)#moet nog thuis en linux kaarten
+install.packages("")library()
+library()
+#
+https://miktex.org/download
+#
+sessionInfo()# geeft versie nummer aan e.d van R
+library()
+#kolom=variabele=attribute
+setwd("G:/")
+library()
+# download R base
+https://cran.r-project.org/bin/windows/base/
+  # download r studio
+  https://www.rstudio.com/products/rstudio/download/
